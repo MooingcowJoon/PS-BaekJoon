f863bcc6eb9770b2da47c1744ff1b9a81f44dc86
MooingcowJoon <130448178+MooingcowJoon@users.noreply.github.com>
2024-05-15 19:36:52 +0900

    [Gold I] Title: 트리의 순회, Time: 752 ms, Memory: 86780 KB -BaekjoonHub

---
+++ 백준/Gold/2263. 트리의 순회/트리의 순회.java
@@ -0,0 +1,50 @@
+import org.w3c.dom.Node;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+public class Main {
+    static int[] in;
+    static int[] post;
+    static ArrayList<Integer> res;
+    static Map<Integer,Integer> map;
+    static Map<Integer,Integer> map2;
+    static void dac(int s, int e,int k){
+        int root = post[e-k];
+        res.add(root);
+        int rootIdx = map.get(root);
+        if(s<rootIdx ){
+                dac(s,rootIdx-1,k);
+        }
+        if(rootIdx<e ){
+                dac(rootIdx+1,e,k+1);
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        map= new HashMap<>();
+        map2= new HashMap<>();
+        res = new ArrayList<>();
+//        in = Arrays.stream(br.readLine().split(" "))
+//                             .map(Integer::parseInt).collect(Collectors.toCollection(LinkedList::new));
+        in = Arrays.stream(br.readLine().split(" "))
+                             .mapToInt(Integer::parseInt).toArray();
+        post = Arrays.stream(br.readLine().split(" "))
+                             .mapToInt(Integer::parseInt).toArray();
+//        post = Arrays.stream(br.readLine().split(" "))
+//                             .map(Integer::parseInt).collect(Collectors.toCollection(LinkedList::new));
+        for(int i=0; i<n; i++){
+            map.put(in[i],i);
+            map2.put(post[i],i);
+        }
+
+
+        dac(0,n-1,0);
+        System.out.println(res.stream().map(Object::toString).collect(Collectors.joining(" ")));
+    }
+}
